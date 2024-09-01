edebccdce58b957f0fe90736db0e21e12a57249b
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-01 12:10:10 +0500

    feat: add HandleError function

--- controllers/back/category.go
+++ controllers/back/category.go
@@ -1,5 +1,27 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"nesil_coffe/config"
+	"nesil_coffe/helpers"
+	"nesil_coffe/models"
 
-func CreateCategory(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+)
+
+func CreateCategory(c *gin.Context) {
+	// initialize database connection
+	db, err := config.ConnDB()
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+	defer db.Close()
+
+	// request body - dan gelen maglumatlar alynyar
+	var category models.Category
+	if err := c.BindJSON(&category); err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+}
