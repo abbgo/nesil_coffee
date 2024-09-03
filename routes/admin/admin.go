ea657da875eafb931c417e42ac9333b206c9c8b3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-09-03 12:31:16 +0500

    some commit

--- routes/admin/admin.go
+++ routes/admin/admin.go
@@ -18,17 +18,5 @@ func AdminRoutes(back *gin.RouterGroup) {
 
 		// UpdateAdmin admin - in maglumatlaryny uytgetmek ucin ulanylyar.
 		admin.PUT("update", middlewares.CheckAdmin(), controllers.UpdateAdmin)
-
-		// // UpdateAdminPassword admin - in parolyny uytgetmek ucin ulanylyar.
-		// admin.PUT("update-password", middlewares.IsSuperAdmin(), controllers.UpdateAdminPassword)
-
-		// // GetAdmin funksiya haeder - den gelen id boyunca bir sany admin - i almak ucin ulanylyar.
-		// admin.GET("one", middlewares.CheckToken("admin"), controllers.GetAdmin)
-
-		// // GetAdmins funksiya hemme admin - leri almak ucin ulanylyar.
-		// admin.GET("", middlewares.IsSuperAdmin(), controllers.GetAdmins)
-
-		// // DeletePermanentlyAdminByID funksiya id boyunca admin - i doly ( korzinadan ) pozmak ucin ulanylyar
-		// admin.DELETE(":id/delete", middlewares.IsSuperAdmin(), controllers.DeletePermanentlyAdminByID)
 	}
 }
