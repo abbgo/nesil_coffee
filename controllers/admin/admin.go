3871e316bf70851b1047009168445302dddf72ed
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-09-03 12:05:23 +0500

    feat: add RegisterAdmin function

---
+++ controllers/admin/admin.go
@@ -0,0 +1,52 @@
+package controllers
+
+import (
+	"context"
+	"nesil_coffe/config"
+	"nesil_coffe/helpers"
+	"nesil_coffe/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterAdmin(c *gin.Context) {
+	db, err := config.ConnDB()
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+	defer db.Close()
+
+	// request - den gelen maglumatlar alynyar
+	var admin models.Admin
+	if err := c.BindJSON(&admin); err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+	if err := models.ValidateAdmin(admin, true); err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+	// parol hashlenyan
+	hashPassword, err := helpers.HashPassword(admin.Password)
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+	// hemme zat yerbe yer bolsa maglumatlar admins tablisa gosulyar
+	_, err = db.Exec(context.Background(), "INSERT INTO admins (full_name,phone_number,password,is_super_admin) VALUES ($1,$2,$3,$4)", admin.FullName, admin.PhoneNumber, hashPassword, admin.IsSuperAdmin)
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":       true,
+		"phone_number": admin.PhoneNumber,
+		"full_name":    admin.FullName,
+	})
+}
