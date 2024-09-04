2cf250445d9a06206049f0f7f9bd10ea2f4f6531
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-04 14:02:45 +0500

    feat: add CategoryID column to CategoryQuery struct

--- serializations/category.go
+++ serializations/category.go
@@ -4,6 +4,7 @@ import "nesil_coffe/helpers"
 
 type CategoryQuery struct {
 	helpers.StandartQuery
-	Search string `form:"search"`
-	Lang   string `form:"lang"`
+	Search     string `form:"search"`
+	Lang       string `form:"lang"`
+	CategoryID string `form:"category_id"`
 }
