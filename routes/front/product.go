34f29203168488a096cb326dded911d78f570f33
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-04 07:02:21 +0500

    feat: add GetProductByID function

--- routes/front/product.go
+++ routes/front/product.go
@@ -9,12 +9,11 @@ import (
 func ProductFrontRoutes(back *gin.RouterGroup) {
 	api := back.Group("/products")
 	{
-		// // GetBrendBGetProductByIDyID -> id - si boyunca Product - in maglumatlaryny almak ucin ulanylyar
-		// api.GET(":id", controllers.GetProductByID)
+		// GetProductByID -> id - si boyunca Product - in maglumatlaryny almak ucin ulanylyar
+		api.GET(":id", controllers.GetProductByID)
 
 		// GetProducts -> Ahli Product - laryn maglumatlaryny request query - den gelen
 		// limit we page boyunca pagination ulanyp almak ucin ulanylyar
 		api.GET("", controllers.GetProducts)
-
 	}
 }
