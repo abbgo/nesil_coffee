9a5ca02d4dc3310eab1b803d8139c131c0ef325b
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-05 14:49:28 +0500

    feat: add ProductID column to Mail model

--- models/mail.go
+++ models/mail.go
@@ -1,8 +1,9 @@
 package models
 
 type ForMail struct {
-	ID       string `json:"id,omitempty"`
-	FullName string `json:"full_name" binding:"required,min=3"`
-	Email    string `json:"email" binding:"email"`
-	Letter   string `json:"letter" binding:"required,min=3"`
+	ID        string `json:"id,omitempty"`
+	FullName  string `json:"full_name" binding:"required,min=3"`
+	Email     string `json:"email" binding:"email"`
+	Letter    string `json:"letter" binding:"required,min=3"`
+	ProductID string `json:"product_id,omitempty"`
 }
