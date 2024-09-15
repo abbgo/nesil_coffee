d5c3b973584c61cae52e995636de26eec10aebe6
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-15 12:27:27 +0500

    fix: update Slider model

--- models/slider.go
+++ models/slider.go
@@ -1,15 +1,15 @@
 package models
 
 type Slider struct {
-	ID            string `json:"id,omitempty"`
-	Image         string `json:"image" binding:"required"`
-	TitleTM       string `json:"title_tm" binding:"required"`
-	TitleRU       string `json:"title_ru" binding:"required"`
-	TitleEN       string `json:"title_en" binding:"required"`
-	SubTitleTM    string `json:"sub_title_tm,omitempty"`
-	SubTitleRU    string `json:"sub_title_ru,omitempty"`
-	SubTitleEN    string `json:"sub_title_en,omitempty"`
-	DescriptionTM string `json:"description_tm,omitempty"`
-	DescriptionRU string `json:"description_ru,omitempty"`
-	DescriptionEN string `json:"description_en,omitempty"`
+	ID            string    `json:"id,omitempty"`
+	Image         BlurImage `json:"image" binding:"required"`
+	TitleTM       string    `json:"title_tm" binding:"required"`
+	TitleRU       string    `json:"title_ru" binding:"required"`
+	TitleEN       string    `json:"title_en" binding:"required"`
+	SubTitleTM    string    `json:"sub_title_tm,omitempty"`
+	SubTitleRU    string    `json:"sub_title_ru,omitempty"`
+	SubTitleEN    string    `json:"sub_title_en,omitempty"`
+	DescriptionTM string    `json:"description_tm,omitempty"`
+	DescriptionRU string    `json:"description_ru,omitempty"`
+	DescriptionEN string    `json:"description_en,omitempty"`
 }
